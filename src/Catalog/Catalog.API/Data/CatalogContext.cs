baa3f63e75d3e24da23209c8bee0818f345151df
sametirkoren <sametirkoren@gmail.com>
2020-07-18 20:56:49 +0300

    Seeding MongoDB Products Collection

--- src/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -16,6 +16,8 @@ namespace Catalog.API.Data
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
             Products = database.GetCollection<Product>(settings.CollectionName);
+
+            CatalogContextSeed.SeedData(Products);
         }
         public IMongoCollection<Product> Products { get; }
     }
---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,73 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "product-1.png",
+                    Price = 850.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Redmi Note 8 ",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "product-2.png",
+                    Price = 550.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Iphone 6",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "product-3.png",
+                    Price = 350.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S10",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "product-4.png",
+                    Price = 750.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Note 5",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "product-5.png",
+                    Price = 650.00M,
+                    Category = "Smart Phone"
+                },
+            };
+        }
+    }
+}
